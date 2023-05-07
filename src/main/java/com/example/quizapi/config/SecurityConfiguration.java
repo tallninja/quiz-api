c5850f4b328dc5c588092cfe38e88b301ece2d43
tallninja <ernestwambua2@gmail.com>
2023-05-07 17:31:08 +0300

    Changed the logout url

--- src/main/java/com/example/quizapi/config/SecurityConfiguration.java
+++ src/main/java/com/example/quizapi/config/SecurityConfiguration.java
@@ -1,10 +1,8 @@
 package com.example.quizapi.config;
 
-import com.example.quizapi.service.userdetails.JpaUserDetailsService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -34,9 +32,12 @@ public class SecurityConfiguration {
                         auth.requestMatchers("/").permitAll();
                         auth.requestMatchers("/actuator/**").permitAll();
                         auth.requestMatchers("/auth/register").permitAll();
+                        auth.requestMatchers("/auth/logout").permitAll();
                         auth.requestMatchers("/users/**").hasAuthority("ADMIN");
                         auth.anyRequest().authenticated();
+
                 })
+                .logout(configurer -> configurer.logoutUrl("/auth/logout"))
                 .userDetailsService(userDetailsService)
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
